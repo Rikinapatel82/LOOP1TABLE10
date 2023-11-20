b0bce49643b6b2281d68ad6ba6b4d9a1927f35db
Rikinapatel82 <149239614+Rikinapatel82@users.noreply.github.com>
2023-11-20 19:42:39 +0000

    Add files via upload

---
+++ Table10.java
@@ -0,0 +1,10 @@
+package table10;
+
+public class Table10 {
+    public static void main(String[] args) {
+       for ( int i =2   ; i<=10;i++)
+        {
+            System.out.println( " 10 * " +i + "= " + i*10); i++;
+        }
+    }
+}
